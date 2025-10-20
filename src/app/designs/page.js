30fdf12c21e8f16583df5b19c15a43a3e4c16de2
Mohmdbilal <mohmdzbilal@gmail.com>
2025-10-20 14:46:10 +0530

    add new

--- src/app/designs/page.js
+++ src/app/designs/page.js
@@ -6,12 +6,7 @@ export default function CreativeDesignPage() {
   const router = useRouter();
 
   const projects = [
-    {
-      title: "Digital Marketing Agency Website Concept",
-      image: "images/work/digitalm.png",
-      figmaLink:
-        "https://www.figma.com/design/S3Fb29FD2VTGrwHGplX4Lp/OG.in?node-id=0-1&t=rCmfcqlMMtLIjHgd-1",
-    },
+
     {
       title: "My-Portfolio",
       image: "images/work/designportfolio.png",
